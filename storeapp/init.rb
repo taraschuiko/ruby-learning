7ca2f90f0bb837bef582d9bbc468faab42cf96a3
Taras Chuiko <taraschuiko@gmail.com>
2020-06-14 09:12:57 +0300

    Completed lesson 27

--- storeapp/init.rb
+++ storeapp/init.rb
@@ -3,13 +3,7 @@ require_relative "order"
 require_relative "real_item"
 require_relative "virtual_item"
 
-item1 = RealItem.new({:price => 110, :weight => 100, :name => "Car"})
-item2 = VirtualItem.new({:price => 10, :name => "Car"})
-item3 = RealItem.new({:price => 20, :weight => 200, :name => "Car"})
-
-cart = Cart.new
-cart.add_item item1
-cart.add_item item2
-cart.add_item item3
-
-puts cart.items.size
+@items = []
+@items << RealItem.new({:price => 110, :weight => 100, :name => "car"})
+@items << VirtualItem.new({:price => 10, :name => "kettle"})
+@items << RealItem.new({:price => 200, :weight => 200, :name => "bed"})
