885e591eb44c030e00f5886dd7dc94c7202f50fe
Taras Chuiko <taraschuiko@gmail.com>
2020-06-14 10:07:39 +0300

    Completed lesson 30, exceprions

--- storeapp/cart.rb
+++ storeapp/cart.rb
@@ -20,9 +20,11 @@ class Cart
   end
 
   def read_from_file
-    return unless File.exists?("#{@owner}_cart.txt")
     File.readlines("#{@owner}_cart.txt").each { |i| @items << i.to_real_item }
     @items.uniq!
+  rescue Errno::ENOENT
+    File.open("#{@owner}_cart.txt") {}
+    puts "File #{@owner}_cart.txt created"
   end
 
 end
